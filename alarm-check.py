cad305ef64d767364e57f00acc22058f1b246a9b
Soma Holiday <soma@somaholiday.com>
2020-11-14 10:00:07 -0800

    Make alarm-check.py read JSON and respect enabled

--- alarm-check.py
+++ alarm-check.py
@@ -2,18 +2,27 @@
 from datetime import datetime
 import subprocess
 import os
+import json
 
 ALARM_PATH = '%s/alarm/alarm.py' % os.path.dirname(os.path.abspath(__file__))
-CONFIG_PATH = '%s/config.txt' % os.path.dirname(os.path.abspath(__file__))
+CONFIG_PATH = '%s/config.json' % os.path.dirname(os.path.abspath(__file__))
 
 now = datetime.now()
 
 current_time = now.strftime("%H:%M")
 
 with open(CONFIG_PATH) as reader:
-    target_time = reader.read()
+    config_json = reader.read()
+    config = json.loads(config_json)
+
+    target_time = config['time']
+    enabled = config['enabled']
 
     print(target_time, current_time)
+
+    if not enabled:
+      print("Not enabled")
+      exit()
 
     if current_time == target_time:
       print("SAME TIME!")
